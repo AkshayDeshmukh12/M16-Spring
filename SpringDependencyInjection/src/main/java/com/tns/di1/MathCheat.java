c20dfd2d7ed2f1106010d14c58783e3ce0d91dd8
Dell <Dell@DESKTOP-6P8TEK1>
2022-06-20 14:40:49 +0530

    First Commit

---
+++ SpringDependencyInjection/src/main/java/com/tns/di1/MathCheat.java
@@ -0,0 +1,9 @@
+package com.tns.di1;
+
+public class MathCheat
+{
+	public void MathCheat()
+	{
+		System.out.println("Macroxerox is reddy....!");
+	}
+}
---
+++ SpringDependencyInjection/src/main/java/com/tns/di1/Student.java
@@ -0,0 +1,15 @@
+package com.tns.di1;
+
+public class Student 
+{
+	MathCheat m;
+	
+public void setM(MathCheat m) {
+		this.m = m;
+	}
+
+public void cheating()
+{
+	m.MathCheat();
+}
+}
